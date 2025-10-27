4d435de9d5981dc9e9a8f0a9eac31cd9d8d5c902
Vithurshan2002 <sangervithu2002@gmail.com>
2025-10-27 15:34:16 +0530

    fronenddepolyed url added

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 app.use(cors());
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://simplechat-rjbc.onrender.com/",
     methods: ["POST", "GET"],
   },
 });
